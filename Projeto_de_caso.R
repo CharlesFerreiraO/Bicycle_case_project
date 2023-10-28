b4333a978d29c24b65ee215ce1bb576ca43b868d
CharlesFerreiraO <charlescosta.fe@gmail.com>
2023-10-28 12:04:33 -0300

    Update Projeto_de_caso.R

--- Projeto_de_caso.R
+++ Projeto_de_caso.R
@@ -1,6 +1,14 @@
-# Código
+# Criando novas variáveis
 
 q2_2019 <- Divvy_Trips_2019_Q2
 q3_2019 <- Divvy_Trips_2019_Q3
 q4_2019 <- Divvy_Trips_2019_Q4
 q1_2020 <- Divvy_Trips_2020_Q1
+
+# Identificando o nome das colunas
+
+colnames(q3_2019)
+colnames(q4_2019)
+colnames(q2_2019)
+colnames(q1_2020)
+
